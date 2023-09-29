b0d3b8f3ba199d2b58db8e7125021f17302de32b
manimanitin <andreman1781@gmail.com>
2023-09-29 11:40:50 -0600

    agregar slidable a items

--- lib/widgets/carga_item.dart
+++ lib/widgets/carga_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/carga.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class CargaItem extends StatelessWidget {
   final CargaElectrica carga;
@@ -8,10 +9,38 @@ class CargaItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(carga.elemento),
-      subtitle: Text('Cantidad: ${carga.cantidad}'),
-      trailing: Text('Energía/día: ${carga.energiaDia.toStringAsFixed(2)} kWh'),
+    return Slidable(
+      startActionPane: ActionPane(
+        // A motion is a widget used to control how the pane animates.
+        motion: const ScrollMotion(),
+
+        // A pane can dismiss the Slidable.
+
+        // All actions are defined in the children parameter.
+        children: [
+          // A SlidableAction can have an icon and/or a label.
+          SlidableAction(
+            onPressed: (context) => 1,
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            onPressed: (context) => 1,
+            backgroundColor: const Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Share',
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(carga.elemento),
+        subtitle: Text('Cantidad: ${carga.cantidad}'),
+        trailing:
+            Text('Energía/día: ${carga.energiaDia.toStringAsFixed(2)} kWh'),
+      ),
     );
   }
 }
