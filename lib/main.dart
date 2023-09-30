c0fcf8603649e04cfbc253da245af00a6e7a4956
Edwin Jesus Santillan Hernandez <deadwiner69@gmail.com>
2023-09-29 18:43:03 -0600

    Splash Screen hecho

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Luz Verde',
       debugShowCheckedModeBanner: false,
+      title: 'Luz Verde',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+
 import 'package:luz_verde_proyecto/screens/home_screen.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,26 +12,59 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
+
   @override
   void initState() {
+    //En esta zona creamos el _controller, donde ponemos la ubicacion del splashScreen
     super.initState();
-    Timer(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-    });
+
+    _controller =
+        VideoPlayerController.asset('assets/animations/FinalSplashScreen.mp4')
+          ..initialize().then((_) {
+            //en el initialize, escribe _controller.play para que se reproduzca el video,
+            _controller.play();
+
+            Timer(const Duration(seconds: 2), () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  //aqui al terminar el video nos redirije a la pantalla home
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
+            });
+
+            setState(() {});
+          });
   }
 
   @override
   Widget build(BuildContext context) {
+    //aqui se hizo lo de centrar el video a la mitad de la pantalla
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: content(),
+        backgroundColor: const Color.fromARGB(255, 233, 232, 232),
+        body: Center(
+          child: content(),
+        ));
+  }
+
+  Widget content() {
+    //aqui se pusieron las dimensiones del video
+    return Stack(
+      children: [
+        SizedBox(
+          child: FittedBox(
+            fit: BoxFit.cover,
+            child: Container(
+              alignment: Alignment.center,
+              width: 1200,
+              height: 1200,
+              child: VideoPlayer(_controller),
+            ),
+          ),
+        )
+      ],
     );
   }
-}
-
-Widget content() {
-  return Center(
-    child: Lottie.asset("assets/animations/aniki_hamster.json"),
-  );
 }
